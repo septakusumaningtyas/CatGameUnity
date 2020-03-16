b05b3543f06055e5fadd05a6eaa0236a7e6fc32b
septakusumaningtyas <septakusumaningtyas@gmail.com>
2020-03-16 19:52:13 +0700

    Button Exit
    Button Exit

---
+++ Assets/Button/QuitButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    public void doQuit()
+    {
+        Application.Quit();
+    }
+}
