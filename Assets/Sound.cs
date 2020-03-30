1a085f450b02201967fe2947b8119b4198257999
septakusumaningtyas <septakusumaningtyas@gmail.com>
2020-03-30 20:35:06 +0700

    Sound
    Sound

---
+++ Assets/Sound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    private static Sound instance = null;
+
+    public static Sound Instance
+    {
+        get { return instance; }
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+}
