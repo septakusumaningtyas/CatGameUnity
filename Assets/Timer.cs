13e2e220b1cf1897adbb085cfdb3ed8cc5c37294
septakusumaningtyas <septakusumaningtyas@gmail.com>
2020-03-30 15:03:08 +0700

    Timer
    Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float timer;
+    float seconds;
+    float minutes;
+
+    [SerializeField] Text timerText;
+    
+    void Start()
+    {
+        timer = 0;
+    }
+    
+    void Update()
+    {
+        TimerWatch();
+    }
+
+    void TimerWatch()
+    {
+        timer += Time.deltaTime;
+        seconds = timer % 60;
+        minutes = timer / 60;
+
+        timerText.text = minutes.ToString("00") + " : " + seconds.ToString("00");
+    }
+}
